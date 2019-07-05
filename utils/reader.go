c52b3711e5007cc6ed2f13ef1ad4bdcd2f9d62a3
Tom Wiesing <tkw01536@gmail.com>
2019-07-06 00:16:18 +0200

    Initial commit

---
+++ utils/reader.go
@@ -0,0 +1,326 @@
+package utils
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"regexp"
+	"strings"
+)
+
+// RuneReader represents something that can read runes from a bufio.Reader
+// but handles \r\n and \n\r as a single '\n'
+type RuneReader struct {
+	Reader   *bufio.Reader
+	position ReaderPosition
+
+	// caching
+	hasCache bool // is the cache valid
+	cache    rune
+}
+
+// ReaderPosition represents the position of the reader
+type ReaderPosition struct {
+	// current 0-based line
+	Line uint
+
+	// current 0-based column
+	Column uint
+
+	// true iff we are at the end of the input
+	EOF bool
+}
+
+// NewRuneReaderFromString creates a new RuneReader from a string
+func NewRuneReaderFromString(s string) *RuneReader {
+	return &RuneReader{
+		Reader: bufio.NewReader(strings.NewReader(s)),
+	}
+}
+
+// NewRuneReaderFromReader creates a new RuneReader from an io.Reader
+func NewRuneReaderFromReader(rd io.Reader) *RuneReader {
+	return &RuneReader{
+		Reader: bufio.NewReader(rd),
+	}
+}
+
+// GetPosition returns the current position of the reader
+func (raw *RuneReader) GetPosition() ReaderPosition {
+	return raw.position
+}
+
+// Next reads the next character
+func (raw *RuneReader) Read() (r rune, pos ReaderPosition, err error) {
+	// catch the end-of line
+	var eof bool
+	defer func() {
+		// if something went wrong, don't change the state
+		if err != nil {
+			return
+		}
+
+		// return the current position
+		pos = ReaderPosition{
+			Line:   raw.position.Line,
+			Column: raw.position.Column,
+			EOF:    eof,
+		}
+
+		// and update the position
+
+		if eof {
+			// we are at the end => store eof and don't change position
+			raw.position.EOF = true
+
+		} else if r == '\n' {
+			// we have a newline => set column to 0 and increase line counter
+			raw.position.Column = 0
+			raw.position.Line++
+
+		} else {
+			// else we only increase the column
+			raw.position.Column++
+		}
+	}()
+
+	// read the next rune
+	a, err := raw.readRaw()
+	if err != nil {
+		if err == io.EOF {
+			eof = true
+			err = nil
+		}
+		return
+	}
+
+	// we consider '\n\r' and '\r\n' special newlines that both get collapsed into just an '\n'
+	// since we might have this, we need to have to peek at the next character also
+	if a == '\n' || a == '\r' {
+		var b rune
+		b, err = raw.peekRaw()
+		// if we have an EOF next, this will be re-read
+		if err == io.EOF {
+			r = '\n'
+			eof = false
+			err = nil
+			return
+
+			// if we have an error, pass that along
+		} else if err != nil {
+			eof = false
+			return
+		}
+
+		// we had a real newline
+		if (a == '\n' && b == '\r') || (a == '\r' && b == '\n') {
+			raw.eatRaw() // skip the next character
+			r = '\n'
+			eof = false
+			err = nil
+			return
+		}
+	}
+
+	// return a
+	r = a
+	return
+}
+
+// Eat is like Read, expect that it does not return any values and only an error (if any)
+func (raw *RuneReader) Eat() (err error) {
+	_, _, err = raw.Read()
+
+	return
+}
+
+// Peek peeks into the next character
+func (raw *RuneReader) Peek() (r rune, pos ReaderPosition, err error) {
+	// read the current position
+	pos = raw.GetPosition()
+
+	// peek the next raw character
+	r, err = raw.peekRaw()
+	if err != nil {
+		if err == io.EOF {
+			pos.EOF = true
+			err = nil
+		}
+		return
+	}
+
+	// if we don't have a potentially special character
+	// we can return everything now
+	if !(r == '\n' || r == '\r') {
+		return
+	}
+
+	// else we fallback to read
+	r, pos, err = raw.Read()
+	if err != nil {
+		return
+	}
+
+	// and then unread
+	err = raw.unreadRaw(r, pos, false)
+
+	return
+}
+
+// Unread unreads a character
+func (raw *RuneReader) Unread(r rune, pos ReaderPosition) error {
+	return raw.unreadRaw(r, pos, false)
+}
+
+// readRaw reads the next character, without taking care of special newlines
+// Return err == io.EOF for the end of file
+func (raw *RuneReader) readRaw() (r rune, err error) {
+	if raw.hasCache {
+		// cache and position to return
+		r = raw.cache
+		raw.hasCache = false
+
+		if raw.position.EOF {
+			err = io.EOF
+		}
+
+		return
+	}
+
+	r, _, err = raw.Reader.ReadRune()
+	return
+}
+
+// eatRaw eats the next character, without taking care of special newlines
+func (raw *RuneReader) eatRaw() (err error) {
+	if raw.hasCache {
+		raw.hasCache = false
+		return
+	}
+	_, _, err = raw.Reader.ReadRune()
+	return
+}
+
+// PeekRaw peeks the next character without taking care of special newlines.
+func (raw *RuneReader) peekRaw() (r rune, err error) {
+	// if we have a cache, return it
+	if raw.hasCache {
+		r = raw.cache
+		return
+	}
+
+	// if we do not have this, return
+	r, _, err = raw.Reader.ReadRune()
+	if err != nil {
+		return
+	}
+	err = raw.Reader.UnreadRune()
+	return
+}
+
+// unreadRaw unreads a character
+// When unsafe is true, the check for already having a cached character is skipped
+func (raw *RuneReader) unreadRaw(r rune, pos ReaderPosition, unsafe bool) (err error) {
+	if raw.hasCache && !unsafe {
+		return errors.New("Cannot unread: Character already cached. ")
+	}
+
+	raw.hasCache = true
+	raw.cache = r
+	raw.position = pos
+
+	return
+}
+
+// ReadWhile reads runes from a string as long as they and the partial string so far match the regexp
+// returns the matching string, the position of the first character of the string, and the last position of the string
+// an empty string implies that the first read character did not match, and that start == end
+func (raw *RuneReader) ReadWhile(f func(r rune) bool) (s string, start ReaderPosition, end ReaderPosition, err error) {
+	// make a buffer for the string
+	var builder strings.Builder
+	defer func() {
+		if err == nil {
+			s = builder.String()
+		}
+	}()
+
+	// read start position
+	start = raw.GetPosition()
+	end = start
+
+	// keep reading the current rune
+	// as long as there is no EOF
+	var r rune
+	var p ReaderPosition
+	for true {
+		r, p, err = raw.Read()
+		if err != nil {
+			return
+		}
+
+		// if we reached EOF, we don't need to check anything
+		if p.EOF {
+			end = p
+			return
+		}
+
+		// if f no longer matches
+		// unread and return
+		if !f(r) {
+			err = raw.Unread(r, p)
+			return
+		}
+
+		// don't add the trailing EOF
+		builder.WriteRune(r)
+		end = p
+	}
+
+	return
+}
+
+// ReadWhileMatch reads runes as long as re matches the characters read up to this point
+// When not in an error state, it is guaranteed that either re.MatchString(s) is true or s is empty and start === end
+func (raw *RuneReader) ReadWhileMatch(re *regexp.Regexp) (s string, start ReaderPosition, end ReaderPosition, err error) {
+	var builder strings.Builder
+	return raw.ReadWhile(func(r rune) bool {
+		builder.WriteRune(r)
+		return re.MatchString(builder.String())
+	})
+}
+
+// EatWhile eats runes from the RuneReader as long as f returns true
+// The first rune for which f does not match is not eaten
+func (raw *RuneReader) EatWhile(f func(r rune) bool) (count int, err error) {
+	var r rune
+	var p ReaderPosition
+	for true {
+		r, p, err = raw.Read()
+		if err != nil {
+			return
+		}
+
+		// if we reached EOF, we don't need to check anything
+		if p.EOF {
+			return
+		}
+
+		// if f no longer matches, we unread and return
+		if !f(r) {
+			err = raw.Unread(r, p)
+			return
+		}
+
+		count++
+	}
+
+	return
+}
+
+// EatWhileMatch eats characters from RuneReader as long as re matches the substring so far
+// the first substring for which the substring does not match is not eaten
+func (raw *RuneReader) EatWhileMatch(re *regexp.Regexp) (count int, err error) {
+	s, _, _, err := raw.ReadWhileMatch(re)
+	count = len(s)
+	return
+}
---
+++ utils/reader_test.go
@@ -0,0 +1,486 @@
+package utils
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+	"testing"
+)
+
+const testInput = "line 1\nline 2\r\nline 3\n\rline 4\nline 5\ralso\r\rstill\n\nline 7\n"
+
+// makeTestReader makes a new test reader
+func makeTestReader() *RuneReader {
+	return NewRuneReaderFromString(testInput)
+}
+
+// the testOutput we are expecting
+var testOutput = []struct {
+	wantR   rune
+	wantPos ReaderPosition
+}{
+	{'l', ReaderPosition{0, 0, false}},
+	{'i', ReaderPosition{0, 1, false}},
+	{'n', ReaderPosition{0, 2, false}},
+	{'e', ReaderPosition{0, 3, false}},
+	{' ', ReaderPosition{0, 4, false}},
+	{'1', ReaderPosition{0, 5, false}},
+	{'\n', ReaderPosition{0, 6, false}},
+
+	{'l', ReaderPosition{1, 0, false}},
+	{'i', ReaderPosition{1, 1, false}},
+	{'n', ReaderPosition{1, 2, false}},
+	{'e', ReaderPosition{1, 3, false}},
+	{' ', ReaderPosition{1, 4, false}},
+	{'2', ReaderPosition{1, 5, false}},
+	{'\n', ReaderPosition{1, 6, false}},
+
+	{'l', ReaderPosition{2, 0, false}},
+	{'i', ReaderPosition{2, 1, false}},
+	{'n', ReaderPosition{2, 2, false}},
+	{'e', ReaderPosition{2, 3, false}},
+	{' ', ReaderPosition{2, 4, false}},
+	{'3', ReaderPosition{2, 5, false}},
+	{'\n', ReaderPosition{2, 6, false}},
+
+	{'l', ReaderPosition{3, 0, false}},
+	{'i', ReaderPosition{3, 1, false}},
+	{'n', ReaderPosition{3, 2, false}},
+	{'e', ReaderPosition{3, 3, false}},
+	{' ', ReaderPosition{3, 4, false}},
+	{'4', ReaderPosition{3, 5, false}},
+	{'\n', ReaderPosition{3, 6, false}},
+
+	{'l', ReaderPosition{4, 0, false}},
+	{'i', ReaderPosition{4, 1, false}},
+	{'n', ReaderPosition{4, 2, false}},
+	{'e', ReaderPosition{4, 3, false}},
+	{' ', ReaderPosition{4, 4, false}},
+	{'5', ReaderPosition{4, 5, false}},
+	{'\r', ReaderPosition{4, 6, false}},
+
+	{'a', ReaderPosition{4, 7, false}},
+	{'l', ReaderPosition{4, 8, false}},
+	{'s', ReaderPosition{4, 9, false}},
+	{'o', ReaderPosition{4, 10, false}},
+
+	{'\r', ReaderPosition{4, 11, false}},
+	{'\r', ReaderPosition{4, 12, false}},
+
+	{'s', ReaderPosition{4, 13, false}},
+	{'t', ReaderPosition{4, 14, false}},
+	{'i', ReaderPosition{4, 15, false}},
+	{'l', ReaderPosition{4, 16, false}},
+	{'l', ReaderPosition{4, 17, false}},
+
+	{'\n', ReaderPosition{4, 18, false}},
+	{'\n', ReaderPosition{5, 0, false}},
+
+	{'l', ReaderPosition{6, 0, false}},
+	{'i', ReaderPosition{6, 1, false}},
+	{'n', ReaderPosition{6, 2, false}},
+	{'e', ReaderPosition{6, 3, false}},
+	{' ', ReaderPosition{6, 4, false}},
+	{'7', ReaderPosition{6, 5, false}},
+
+	{'\n', ReaderPosition{6, 6, false}},
+
+	{rune(0), ReaderPosition{7, 0, true}},
+	{rune(0), ReaderPosition{7, 0, true}},
+}
+
+func TestRuneReader_Read(t *testing.T) {
+
+	raw := makeTestReader()
+
+	for i, tt := range testOutput {
+		t.Run(fmt.Sprintf("read character %d", i), func(t *testing.T) {
+			// read the current position
+			gotPos := raw.GetPosition()
+			gotPos.EOF = tt.wantPos.EOF // ignore EOF (as this may be different)
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.GetPosition() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			gotR, gotPos, err := raw.Read()
+			if (err != nil) != false {
+				t.Errorf("RuneReader.Read() error = %v, wantErr %v", err, false)
+				return
+			}
+			if gotR != tt.wantR {
+				t.Errorf("RuneReader.Read() gotR = %v, want %v", gotR, tt.wantR)
+			}
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.Read() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+		})
+	}
+}
+
+func TestRuneReader_PeekEat(t *testing.T) {
+
+	raw := makeTestReader()
+
+	for i, tt := range testOutput {
+		t.Run(fmt.Sprintf("peek + eat character %d", i), func(t *testing.T) {
+
+			// read the current position
+			gotPos := raw.GetPosition()
+			gotPos.EOF = tt.wantPos.EOF // ignore EOF (as this may be different)
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.GetPosition() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// peek the next character
+			gotR, gotPos, err := raw.Peek()
+			if (err != nil) != false {
+				t.Errorf("RuneReader.Peek() error = %v, wantErr %v", err, false)
+				return
+			}
+			if gotR != tt.wantR {
+				t.Errorf("RuneReader.Peek() gotR = %v, want %v", gotR, tt.wantR)
+			}
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.Peek() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// read the current position (again)
+			gotPos = raw.GetPosition()
+			gotPos.EOF = tt.wantPos.EOF // ignore EOF (as this may be different)
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.GetPosition() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// eat
+			err = raw.Eat()
+			if (err != nil) != false {
+				t.Errorf("RuneReader.Eat() error = %v, wantErr %v", err, false)
+				return
+			}
+
+		})
+	}
+}
+
+func TestRuneReader_ReadUnread(t *testing.T) {
+
+	raw := makeTestReader()
+
+	for i, tt := range testOutput {
+		t.Run(fmt.Sprintf("read + unread + reread character %d", i), func(t *testing.T) {
+
+			// read the current position
+			gotPos := raw.GetPosition()
+			gotPos.EOF = tt.wantPos.EOF // ignore EOF (as this may be different)
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.GetPosition() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// read the next character
+			gotR, gotPos, err := raw.Read()
+			if (err != nil) != false {
+				t.Errorf("RuneReader.Read() error = %v, wantErr %v", err, false)
+				return
+			}
+			if gotR != tt.wantR {
+				t.Errorf("RuneReader.Read() gotR = %v, want %v", gotR, tt.wantR)
+			}
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.Read() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// unread it
+			err = raw.Unread(gotR, gotPos)
+			if (err != nil) != false {
+				t.Errorf("RuneReader.Unread() error = %v, wantErr %v", err, false)
+				return
+			}
+
+			// read the current position (again)
+			gotPos = raw.GetPosition()
+			gotPos.EOF = tt.wantPos.EOF // ignore EOF (as this may be different)
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.GetPosition() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// re-read the next character
+			gotR, gotPos, err = raw.Read()
+			if (err != nil) != false {
+				t.Errorf("RuneReader.Read() error = %v, wantErr %v", err, false)
+				return
+			}
+			if gotR != tt.wantR {
+				t.Errorf("RuneReader.Read() gotR = %v, want %v", gotR, tt.wantR)
+			}
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.Read() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+		})
+	}
+}
+
+func TestRuneReader_ReadUnreadPeekEat(t *testing.T) {
+
+	raw := makeTestReader()
+
+	for i, tt := range testOutput {
+		t.Run(fmt.Sprintf("read + unread + peek + eat character %d", i), func(t *testing.T) {
+			// read the current position
+			gotPos := raw.GetPosition()
+			gotPos.EOF = tt.wantPos.EOF // ignore EOF (as this may be different)
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.GetPosition() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// read the next character
+			gotR, gotPos, err := raw.Read()
+			if (err != nil) != false {
+				t.Errorf("RuneReader.Read() error = %v, wantErr %v", err, false)
+				return
+			}
+			if gotR != tt.wantR {
+				t.Errorf("RuneReader.Read() gotR = %v, want %v", gotR, tt.wantR)
+			}
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.Read() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// unread it
+			err = raw.Unread(gotR, gotPos)
+			if (err != nil) != false {
+				t.Errorf("RuneReader.Unread() error = %v, wantErr %v", err, false)
+				return
+			}
+
+			// read the current position (again)
+			gotPos = raw.GetPosition()
+			gotPos.EOF = tt.wantPos.EOF // ignore EOF (as this may be different)
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.GetPosition() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// re-peak the next character
+			gotR, gotPos, err = raw.Peek()
+			if (err != nil) != false {
+				t.Errorf("RuneReader.Peek() error = %v, wantErr %v", err, false)
+				return
+			}
+			if gotR != tt.wantR {
+				t.Errorf("RuneReader.Peek() gotR = %v, want %v", gotR, tt.wantR)
+			}
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.Peek() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// read the current position (again again)
+			gotPos = raw.GetPosition()
+			gotPos.EOF = tt.wantPos.EOF // ignore EOF (as this may be different)
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.GetPosition() gotPOS = %v, want %v", gotPos, tt.wantPos)
+			}
+
+			// and eat it
+			err = raw.Eat()
+			if (err != nil) != false {
+				t.Errorf("RuneReader.Eat() error = %v, wantErr %v", err, false)
+				return
+			}
+		})
+	}
+}
+
+func TestRuneReader_ReadWhile(t *testing.T) {
+	tests := []struct {
+		name      string
+		f         func(r rune) bool
+		wantS     string
+		wantStart ReaderPosition
+		wantEnd   ReaderPosition
+	}{
+		{
+			"read nothing",
+			func(r rune) bool { return false },
+			"",
+			ReaderPosition{0, 0, false},
+			ReaderPosition{0, 0, false},
+		},
+		{
+			"read letters",
+			func(r rune) bool { return (r >= 'a' && r <= 'z') },
+			"line",
+			ReaderPosition{0, 0, false},
+			ReaderPosition{0, 3, false},
+		},
+		{
+			"read until the end",
+			func(r rune) bool { return true },
+			"line 1\nline 2\nline 3\nline 4\nline 5\ralso\r\rstill\n\nline 7\n",
+			ReaderPosition{0, 0, false},
+			ReaderPosition{7, 0, true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			raw := makeTestReader()
+			gotS, gotStart, gotEnd, err := raw.ReadWhile(tt.f)
+			if (err != nil) != false {
+				t.Errorf("RuneReader.ReadWhile() error = %v, wantErr %v", err, false)
+				return
+			}
+			if gotS != tt.wantS {
+				t.Errorf("RuneReader.ReadWhile() gotS = %v, want %v", gotS, tt.wantS)
+			}
+			if !reflect.DeepEqual(gotStart, tt.wantStart) {
+				t.Errorf("RuneReader.ReadWhile() gotStart = %v, want %v", gotStart, tt.wantStart)
+			}
+			if !reflect.DeepEqual(gotEnd, tt.wantEnd) {
+				t.Errorf("RuneReader.ReadWhile() gotEnd = %v, want %v", gotEnd, tt.wantEnd)
+			}
+		})
+	}
+}
+
+func TestRuneReader_ReadWhileMatch(t *testing.T) {
+	tests := []struct {
+		name      string
+		re        *regexp.Regexp
+		wantS     string
+		wantStart ReaderPosition
+		wantEnd   ReaderPosition
+	}{
+		{
+			"read nothing",
+			regexp.MustCompile("^$"),
+			"",
+			ReaderPosition{0, 0, false},
+			ReaderPosition{0, 0, false},
+		},
+		{
+			"read letters",
+			regexp.MustCompile("^[aA-zZ]+$"),
+			"line",
+			ReaderPosition{0, 0, false},
+			ReaderPosition{0, 3, false},
+		},
+		{
+			"read until the end",
+			regexp.MustCompile(".*"),
+			"line 1\nline 2\nline 3\nline 4\nline 5\ralso\r\rstill\n\nline 7\n",
+			ReaderPosition{0, 0, false},
+			ReaderPosition{7, 0, true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			raw := makeTestReader()
+
+			gotS, gotStart, gotEnd, err := raw.ReadWhileMatch(tt.re)
+			if (err != nil) != false {
+				t.Errorf("RuneReader.ReadWhileMatch() error = %v, wantErr %v", err, false)
+				return
+			}
+			if gotS != tt.wantS {
+				t.Errorf("RuneReader.ReadWhileMatch() gotS = %v, want %v", gotS, tt.wantS)
+			}
+			if !reflect.DeepEqual(gotStart, tt.wantStart) {
+				t.Errorf("RuneReader.ReadWhileMatch() gotStart = %v, want %v", gotStart, tt.wantStart)
+			}
+			if !reflect.DeepEqual(gotEnd, tt.wantEnd) {
+				t.Errorf("RuneReader.ReadWhileMatch() gotEnd = %v, want %v", gotEnd, tt.wantEnd)
+			}
+		})
+	}
+}
+
+func TestRuneReader_EatWhile(t *testing.T) {
+	tests := []struct {
+		name      string
+		f         func(r rune) bool
+		wantCount int
+		wantPos   ReaderPosition
+	}{
+		{
+			"eat nothing",
+			func(r rune) bool { return false },
+			0,
+			ReaderPosition{0, 0, false},
+		},
+		{
+			"eat letters",
+			func(r rune) bool { return (r >= 'a' && r <= 'z') },
+			4,
+			ReaderPosition{0, 4, false},
+		},
+		{
+			"eat everything",
+			func(r rune) bool { return true },
+			55,
+			ReaderPosition{7, 0, true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			raw := makeTestReader()
+			gotCount, gotErr := raw.EatWhile(tt.f)
+			if (gotErr != nil) != false {
+				t.Errorf("RuneReader.EatWhile() error = %v, wantErr %v", gotErr, false)
+			}
+			if gotCount != tt.wantCount {
+				t.Errorf("RuneReader.EatWhile() count = %v, wantCount %v", gotCount, tt.wantCount)
+			}
+
+			gotPos := raw.GetPosition()
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.GetPosition() gotPos = %v, want %v", gotPos, tt.wantPos)
+			}
+		})
+	}
+}
+
+func TestRuneReader_EatWhileMatch(t *testing.T) {
+	tests := []struct {
+		name      string
+		re        *regexp.Regexp
+		wantCount int
+		wantPos   ReaderPosition
+	}{
+		{
+			"eat nothing",
+			regexp.MustCompile("^$"),
+			0,
+			ReaderPosition{0, 0, false},
+		},
+		{
+			"eat letters",
+			regexp.MustCompile("^[aA-zZ]+$"),
+			4,
+			ReaderPosition{0, 4, false},
+		},
+		{
+			"eat everything",
+			regexp.MustCompile(".*"),
+			55,
+			ReaderPosition{7, 0, true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			raw := makeTestReader()
+
+			gotCount, err := raw.EatWhileMatch(tt.re)
+			if (err != nil) != false {
+				t.Errorf("RuneReader.EatWhileMatch() error = %v, wantErr %v", err, false)
+				return
+			}
+			if gotCount != tt.wantCount {
+				t.Errorf("RuneReader.EatWhileMatch() gotCount = %v, want %v", gotCount, tt.wantCount)
+			}
+
+			gotPos := raw.GetPosition()
+			if !reflect.DeepEqual(gotPos, tt.wantPos) {
+				t.Errorf("RuneReader.GetPosition() gotPos = %v, want %v", gotPos, tt.wantPos)
+			}
+		})
+	}
+}
