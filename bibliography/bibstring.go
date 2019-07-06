52d1f09bc83262d804701808927e77a7064bcd4e
Tom Wiesing <tkw01536@gmail.com>
2019-07-06 22:42:34 +0200

    Implement initial BibString datastructure

---
+++ bibliography/bibstring.go
@@ -0,0 +1,79 @@
+package bibliography
+
+import (
+	"strings"
+
+	"github.com/tkw1536/gotexml/utils"
+)
+
+// BibString represents a string along with a reference to a BibFile
+type BibString struct {
+	kind  BibStringKind // the type of Bibstring this is
+	value string        // the value of this bibstring
+
+	// range of this token inside the source file
+	start utils.ReaderPosition
+	end   utils.ReaderPosition
+}
+
+// BibStringKind represents the specific type of BibStrings that can occur
+type BibStringKind string
+
+// kind of BibStrings that occur in the code
+const (
+	BibStringOther     BibStringKind = ""
+	BibStringLiteral   BibStringKind = "LITERAL"
+	BibStringQuote     BibStringKind = "QUOTE"
+	BibStringEvaluated BibStringKind = "EVALUATED"
+)
+
+// Copy makes a copy of this BibString
+func (bs *BibString) Copy() *BibString {
+	// TODO: Check if this is used
+	return &BibString{
+		kind:  bs.kind,
+		value: bs.value,
+		start: bs.start,
+		end:   bs.end,
+	}
+}
+
+// Kind gets the kind of this BibString
+func (bs *BibString) Kind() BibStringKind {
+	return bs.kind
+}
+
+// Value gets the value of this BibString
+func (bs *BibString) Value() string {
+	return bs.value
+}
+
+// NormalizeValue normalizes the value of this BibString
+func (bs *BibString) NormalizeValue() {
+	bs.value = strings.ToLower(bs.value)
+}
+
+// Evaluate evaluates this BibString inside a context
+// returns true iff evaluation was successfull
+func (bs *BibString) Evaluate(context map[string]string) bool {
+	if bs.kind == BibStringLiteral {
+		// grab the value of this string
+		value, ok := context[strings.ToLower(bs.value)]
+		if !ok {
+			return false
+		}
+
+		// update the kind and value
+		bs.kind = BibStringEvaluated
+		bs.value = value
+	}
+	return true
+}
+
+// Append appends the value of another bibstring to this bibstring
+// This function does not check the legality of such an operation
+func (bs *BibString) Append(other *BibString) {
+	bs.kind = BibStringEvaluated
+	bs.value += other.value
+	bs.end = other.end
+}
---
+++ bibliography/bibstring_test.go
@@ -0,0 +1,181 @@
+package bibliography
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/tkw1536/gotexml/utils"
+)
+
+func TestBibString_Copy(t *testing.T) {
+	type fields struct {
+		kind  BibStringKind
+		value string
+		start utils.ReaderPosition
+		end   utils.ReaderPosition
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *BibString
+	}{
+		{"copy a bibstring returns a copy", fields{
+			kind:  BibStringOther,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}, &BibString{
+			kind:  BibStringOther,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bs := &BibString{
+				kind:  tt.fields.kind,
+				value: tt.fields.value,
+				start: tt.fields.start,
+				end:   tt.fields.end,
+			}
+			if got := bs.Copy(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BibString.Copy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBibString_NormalizeValue(t *testing.T) {
+	tests := []struct {
+		name   string
+		before *BibString
+		after  *BibString
+	}{
+		{"normalize simple value", &BibString{value: "HeLlO wOrLd"}, &BibString{value: "hello world"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.before.NormalizeValue()
+			if !reflect.DeepEqual(tt.before, tt.after) {
+				t.Errorf("BibString.NormalizeValue() = %v, want %v", tt.before, tt.after)
+			}
+		})
+	}
+}
+
+func TestBibString_Evaluate(t *testing.T) {
+	tests := []struct {
+		name    string
+		before  *BibString
+		context map[string]string
+
+		wantOK bool
+		after  *BibString
+	}{
+		{"evaluating other bibstring", &BibString{
+			kind:  BibStringOther,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}, map[string]string{"something": "other"}, true, &BibString{
+			kind:  BibStringOther,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}},
+
+		{"evaluating literal bibstring with valid context", &BibString{
+			kind:  BibStringLiteral,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}, map[string]string{"something": "other"}, true, &BibString{
+			kind:  BibStringEvaluated,
+			value: "other",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}},
+
+		{"evaluating literal bibstring with invalid context", &BibString{
+			kind:  BibStringLiteral,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}, nil, false, &BibString{
+			kind:  BibStringLiteral,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}},
+
+		{"evaluating quoted bibstring", &BibString{
+			kind:  BibStringQuote,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}, map[string]string{"something": "other"}, true, &BibString{
+			kind:  BibStringQuote,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}},
+
+		{"evaluating evaluated bibstring", &BibString{
+			kind:  BibStringEvaluated,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}, map[string]string{"something": "other"}, true, &BibString{
+			kind:  BibStringEvaluated,
+			value: "something",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: true},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.before.Evaluate(tt.context); got != tt.wantOK {
+				t.Errorf("BibString.Evaluate() ok = %v, wantOK %v", got, tt.wantOK)
+			}
+			if !reflect.DeepEqual(tt.before, tt.after) {
+				t.Errorf("BibString.Evaluate() = %v, want %v", tt.before, tt.after)
+			}
+		})
+	}
+}
+
+func TestBibString_Append(t *testing.T) {
+	tests := []struct {
+		name   string
+		before *BibString
+		other  *BibString
+
+		after *BibString
+	}{
+		{"adding two strings", &BibString{
+			kind:  BibStringQuote,
+			value: "hello \n",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 2, Column: 0, EOF: false},
+		}, &BibString{
+			kind:  BibStringQuote,
+			value: "world\n",
+			start: utils.ReaderPosition{Line: 2, Column: 1, EOF: false},
+			end:   utils.ReaderPosition{Line: 3, Column: 0, EOF: true},
+		}, &BibString{
+			kind:  BibStringEvaluated,
+			value: "hello \nworld\n",
+			start: utils.ReaderPosition{Line: 0, Column: 0, EOF: false},
+			end:   utils.ReaderPosition{Line: 3, Column: 0, EOF: true},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.before.Append(tt.other)
+			if !reflect.DeepEqual(tt.before, tt.after) {
+				t.Errorf("BibString.Append() = %v, want %v", tt.before, tt.after)
+			}
+		})
+	}
+}
